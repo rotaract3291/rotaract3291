0fcb6a6915e63fb32f8409ea8bcbfdad3261019e
atisayajain <atisayajain@gmail.com>
2021-10-18 22:39:04 +0530

    added role based access - base

--- components/bulletins/AddEdit.jsx
+++ components/bulletins/AddEdit.jsx
@@ -103,7 +103,7 @@ function AddEdit(props) {
 										<label class="block text-gray-700 text-sm font-bold mb-2" for="username">
 											Rotaract Club of
 										</label>
-										<input disabled value={sessionData['clubName']} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+										<input disabled value={clubName} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
 									</div>
 									<div class="mb-4 inline-block relative w-full">
 										<label class="block text-gray-700 text-sm font-bold mb-2" for="username">
