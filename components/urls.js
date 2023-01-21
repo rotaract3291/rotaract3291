93b8a29ee9f244967a808de70d5517f6f3136979
Panfern <varunmodi01@gmail.com>
2023-01-21 17:07:05 +0530

    Updated the api endpoints

--- components/urls.js
+++ components/urls.js
@@ -1,8 +1,8 @@
 export { MEMBERS_API, MEETINGS_API, PROJECTS_API, BULLETINS_API, PARTICIPATIONS_API, COLLABORATIONS_API };
 
-const MEMBERS_API = 'https://nuj520fe23.execute-api.ap-south-1.amazonaws.com/dev';
-const MEETINGS_API = 'https://nuk6ixjpaj.execute-api.ap-south-1.amazonaws.com/dev';
-const PROJECTS_API = 'https://p3pjbzjx9f.execute-api.ap-south-1.amazonaws.com/dev';
-const BULLETINS_API = 'https://qszq0655yk.execute-api.ap-south-1.amazonaws.com/dev';
-const PARTICIPATIONS_API = 'https://m2zrw2en95.execute-api.ap-south-1.amazonaws.com/dev';
-const COLLABORATIONS_API = 'https://frtgn4i0i4.execute-api.ap-south-1.amazonaws.com/dev';
+const MEMBERS_API = 'https://uj8ckiz750.execute-api.ap-south-1.amazonaws.com/dev'; //------------
+const MEETINGS_API = 'https://xund59ygy0.execute-api.ap-south-1.amazonaws.com/dev'; //---------------
+const PROJECTS_API = 'https://b2sfr7jjm6.execute-api.ap-south-1.amazonaws.com/dev'; //-----------------
+const BULLETINS_API = ' https://5t42mzvz84.execute-api.ap-south-1.amazonaws.com/dev'; //-------------------
+const PARTICIPATIONS_API = 'https://80ty11qmqh.execute-api.ap-south-1.amazonaws.com/dev'; //----------------
+const COLLABORATIONS_API = 'https://dgg3emo9lc.execute-api.ap-south-1.amazonaws.com/dev'; //------
